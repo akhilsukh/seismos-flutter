efbaa7e50bb11b9943ef24a011136af0f7d607ea
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-19 13:31:37 -0700

    Create seismolist-page.dart for earthquake list

---
+++ lib/pages/seismolist-page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../components/widgets.dart';
+
+class Seismolist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: Header(title: "Seismolist"));
+  }
+}
