d4f3074225546458eb4ffc33d265d02fd3b8d620
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-20 09:17:38 -0700

    Added Seismoslist listview

--- lib/pages/seismolist-page.dart
+++ lib/pages/seismolist-page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:seismos_flutter/quake-fetch.dart';
+import 'package:seismos_flutter/quake.dart';
 import '../components/widgets.dart';
 
-class Seismolist extends StatelessWidget {
+class Seismolist extends StatefulWidget {
+  @override
+  _SeismolistState createState() => _SeismolistState();
+}
+
+class _SeismolistState extends State<Seismolist> {
+  List<Feature> _quake;
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+    QuakeFetch.getQuakes().then((value) {
+      setState(() {
+        _quake = value.features;
+        _loading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: Header(title: "Seismolist"));
+    return Scaffold(
+      appBar: Header(title: "Seismolist"),
+      body: Container(
+        child: ListView.builder(
+            itemCount: null == _quake ? 0 : _quake.length,
+            itemBuilder: (context, index) {
+              Feature feature = _quake[index];
+              return ListTile(
+                title: Text(feature.properties.place),
+                subtitle: Text("Mag - " + feature.properties.mag.toString()),
+              );
+            }),
+      ),
+    );
   }
 }
