2f3c58f72a97d78dc83dc62470649bcd6c6ea86a
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-20 16:07:25 -0700

    Created basic structure for Preparatory using ImageButtons

--- lib/pages/prep-page.dart
+++ lib/pages/prep-page.dart
@@ -5,6 +5,31 @@ import '../components/widgets.dart';
 class Preparatory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: Header(title: "Preparation Guide"));
+    return Scaffold(
+      appBar: Header(title: "Preparation Guide"),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        child: Column(
+          children: [
+            ImageButton(
+              title: "General",
+              image: AssetImage('assets/map-background.png'),
+            ),
+            ImageButton(
+              title: "Before",
+              image: AssetImage('assets/map-background.png'),
+            ),
+            ImageButton(
+              title: "During",
+              image: AssetImage('assets/map-background.png'),
+            ),
+            ImageButton(
+              title: "After",
+              image: AssetImage('assets/map-background.png'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
