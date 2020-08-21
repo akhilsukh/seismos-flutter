2ffd0105bd1b403f7a973e1282d8baad82841fc5
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-21 14:44:18 -0700

    Added ability to pass custom URL and removed unused imports

--- lib/quake-fetch.dart
+++ lib/quake-fetch.dart
@@ -1,18 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-import 'components/widgets.dart';
 import 'quake.dart';
 
 class QuakeFetch {
-  static const String url =
-      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson";
-
-  static Future<Quake> getQuakes() async {
+  static Future<Quake> getQuakes(String url) async {
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
