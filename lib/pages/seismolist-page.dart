ce1a870e355390ca71a2e2813a78b26f43f036da
Akhil Sukhthankar <akhilsukh01@gmail.com>
2020-08-31 08:59:27 -0700

    Update card style in Seismolist

--- lib/pages/seismolist-page.dart
+++ lib/pages/seismolist-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:seismos_flutter/quake-fetch.dart';
 import 'package:seismos_flutter/quake.dart';
@@ -30,9 +31,25 @@ class _SeismolistState extends State<Seismolist> {
             itemCount: null == _quake ? 0 : _quake.length,
             itemBuilder: (context, index) {
               Feature feature = _quake[index];
-              return ListTile(
-                title: Text(feature.properties.place),
-                subtitle: Text("Mag - " + feature.properties.mag.toString()),
+              return Card(
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        feature.properties.place,
+                        style: GoogleFonts.robotoMono(
+                            textStyle: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500)),
+                      ),
+                      Text(
+                        "Mag - " + feature.properties.mag.toString(),
+                        style: GoogleFonts.robotoMono(),
+                      ),
+                    ],
+                  ),
+                ),
               );
             });
       });
