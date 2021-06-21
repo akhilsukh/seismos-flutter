5f3ec0bc8bb5c0943313a8ebff9cc8dcd53375f3
Akhil Sukhthankar <akhilsukh01@gmail.com>
2021-06-20 17:13:36 -0700

    quick updates

---
+++ android/app/src/main/kotlin/com/akhilsukh01/seismos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.akhilsukh01.seismos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/akhilsukh01/seismos_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.akhilsukh01.seismos_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
